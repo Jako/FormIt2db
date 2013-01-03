6b0d2766b679790fe1206bb10019d5b0248eef91
Thomas Jakobi <thomas.jakobi@partout.info>
2013-01-03 14:14:04 +0100

    Initial public release

--- core/components/formit2db/lexicon/en/properties.inc.php
+++ core/components/formit2db/lexicon/en/properties.inc.php
@@ -26,22 +26,22 @@
  *
  * FormIt2db/db2FormIt
  */
-$_lang['prop_formit2db.prefix'] = 'Table prefix of the XPDO package';
-$_lang['prop_formit2db.packagename'] = 'Package name of the XPDO package';
-$_lang['prop_formit2db.classname'] = 'Class name of the XPDO object';
-$_lang['prop_formit2db.where'] = 'JSON encoded XPDO where clause - to update a row instead of creating a new one';
+$_lang['prop_formit2db.prefix'] = 'Table prefix of the xPDO package';
+$_lang['prop_formit2db.packagename'] = 'Package name of the xPDO package';
+$_lang['prop_formit2db.classname'] = 'Class name of the xPDO object';
+$_lang['prop_formit2db.where'] = 'JSON encoded xPDO where clause - to update a row instead of creating a new one';
 $_lang['prop_formit2db.paramname'] = 'Requested POST param - to update a row instead of creating a new one';
-$_lang['prop_formit2db.fieldname'] = 'XPDO fieldname the POST param is compared with - to update a row instead of creating a new one';
+$_lang['prop_formit2db.fieldname'] = 'xPDO fieldname the POST param is compared with - to update a row instead of creating a new one';
 $_lang['prop_formit2db.arrayFormat'] = 'Format to transform form fields that contains array data (i.e. checkboxes) into';
 $_lang['prop_formit2db.arrayFields'] = 'JSON encoded array of form fields that contains array data i.e. ["field_1", "field_2"]';
-$_lang['prop_formit2db.removeFields'] = 'JSON encoded array of form fields not saved in the XPDO object i.e. ["field_1", "field_2"]';
+$_lang['prop_formit2db.removeFields'] = 'JSON encoded array of form fields not saved in the xPDO object i.e. ["field_1", "field_2"]';
 
 $_lang['prop_db2formit.prefix'] = $_lang['prop_formit2db.prefix'];
 $_lang['prop_db2formit.packagename'] = $_lang['prop_formit2db.packagename'];
 $_lang['prop_db2formit.classname'] = $_lang['prop_formit2db.classname'];
-$_lang['prop_db2formit.where'] = 'JSON encoded XPDO where clause - to retreive an existing row';
+$_lang['prop_db2formit.where'] = 'JSON encoded xPDO where clause - to retreive an existing row';
 $_lang['prop_db2formit.paramname'] = 'Requested GET param - to retreive an existing row';
-$_lang['prop_db2formit.fieldname'] = 'XPDO fieldname the GET param is compared with - to retreive an existing row';
+$_lang['prop_db2formit.fieldname'] = 'xPDO fieldname the GET param is compared with - to retreive an existing row';
 $_lang['prop_db2formit.arrayFormat'] = 'Format to transform form fields that contains array data (i.e. checkboxes) from';
 $_lang['prop_db2formit.arrayFields'] = 'JSON encoded array of form fields that are transformed into arrays i.e. ["field_1", "field_2"]';
 $_lang['prop_db2formit.notFoundRedirect'] = 'ID of the MODX resource the user is redirected to, if the requested row is not found';
